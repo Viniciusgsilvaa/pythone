634d8937a52d0e5d2e5475240aead23d5406ed7c
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2023-12-15 06:40:05 -0300

    Iniciando meus estudos em PostgresSQL

---
+++ PostgreSQL/secao03.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE secao03;
+
+-- CREATE TABLE tipos_produtos(
+--	id SERIAL PRIMARY KEY,
+--	descricao CHARACTER VARYING(50) NOT NULL
+-- );
+
+-- CREATE TABLE produtos(
+--	id SERIAL PRIMARY KEY,
+--	descricao CHARACTER VARYING(50) NOT NULL,
+--	preco MONEY NOT NULL,
+--	id_tipo_produto INT REFERENCES tipos_produtos(id) NOT NULL
+-- );
