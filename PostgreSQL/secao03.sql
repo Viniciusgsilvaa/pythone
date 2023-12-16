35fa42e43f55da0e569d19e67753e76c4be08324
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2023-12-16 06:29:02 -0300

    Adicionando novas tabelas ao postgres

--- PostgreSQL/secao03.sql
+++ PostgreSQL/secao03.sql
@@ -11,3 +11,26 @@
 --	preco MONEY NOT NULL,
 --	id_tipo_produto INT REFERENCES tipos_produtos(id) NOT NULL
 -- );
+
+-- CREATE TABLE pacientes(
+--	id SERIAL PRIMARY KEY,
+--	nome CHARACTER VARYING(50) NOT NULL,
+--	endere CHARACTER VARYING(50) NOT NULL,
+--	bairro CHARACTER VARYING(50) NOT NULL,
+--	cidade VARCHAR(40) NOT NULL,
+--	estado CHAR(2) NOT NULL,
+--	cep VARCHAR(9) NOT NULL,
+--	data_nascimento DATE NOT NULL
+--	);
+
+-- CREATE TABLE professor(
+--	id SERIAL PRIMARY KEY,
+--	telefone INT NOT NULL,
+--	nome VARCHAR(50) NOT NULL
+-- );
+
+--CREATE TABLE turmas(
+--	id SERIAL PRIMARY KEY,
+--	capacidade INT NOT NULL,
+-- 	id_professor INT REFERENCES professor(id) NOT NULL 
+-- );
