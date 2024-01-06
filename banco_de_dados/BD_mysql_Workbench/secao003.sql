e9d3d7bd801d134771b92934b32fa5a9992d566d
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-01-06 08:31:10 -0300

    adicionando a secao003.sql

---
+++ banco_de_dados/BD_mysql_Workbench/secao003.sql
@@ -0,0 +1,20 @@
+-- USE secao03;
+
+-- SELECT * FROM tipos_produto;
+
+-- INSERT INTO tipos_produto (descricao) VALUES ('Notebook');
+
+-- SELECT * FROM produtos;
+
+-- INSERT INTO `secao03`.`produtos` (`descricao`, `preco`, `id_tipos_produto`) VALUES ('NoSamgsu', '89.99', '2');
+
+-- select * from tipos_produto;
+
+-- insert into tipos_produto (descricao) value ('Video Game');
+
+-- select * from produtos;
+
+-- insert into `secao03`.`produtos` (`descricao`, `preco`, `id_tipos_produto`) values ('Nintendo switch', '349.99', '3');
+
+-- select * from produtos;
+
