46ad0a4d7720edc24ef8cce3598fba43f0448aed
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-01-07 16:07:44 -0300

    Adicionando exercicio secao05

---
+++ banco_de_dados/BD_mysql_Workbench/secao05.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE secao05;
+
+USE secao05;
+
+CREATE TABLE tipos_produto (
+	codigo INT NOT NULL AUTO_INCREMENT,
+    descricao VARCHAR(30) NOT NULL,
+    PRIMARY KEY(codigo)
+);
+
+CREATE TABLE produtos(
+	codigo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    descricao VARCHAR(30) NOT NULL,
+    preco DECIMAL(8,2) NOT NULL,
+    codigo_tipo INT NOT NULL,
+    FOREIGN KEY (codigo_tipo) REFERENCES tipos_produto(codigo)
+);
+
+INSERT INTO tipos_produto (descricao) VALUES ('Computador');
+INSERT INTO tipos_produto (descricao) VALUES ('Impressora');
+
+INSERT INTO produtos (descricao, preco, codigo_tipo) VALUES ('Desktop', '1200', 1);
+INSERT INTO produtos (descricao, preco, codigo_tipo) VALUES ('Laptop', '1800', 1);
+INSERT INTO produtos (descricao, preco, codigo_tipo) VALUES ('Impr. Jato Tinta', '300', 2);
+INSERT INTO produtos (descricao, preco, codigo_tipo) VALUES ('Impr. Laser', '500', 2);
+
+SELECT * FROM produtos WHERE codigo_tipo = 2 and preco > 300;
+
+SELECT p.codigo, p.descricao, p.preco, tp.descricao
+	FROM produtos AS p, tipos_produto AS tp
+    WHERE p.codigo_tipo = tp.codigo;
