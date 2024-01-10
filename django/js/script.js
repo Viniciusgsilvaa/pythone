2c04ae58850f6721d18a842ce155d6066c0515fa
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-01-10 08:18:22 -0300

    Exercicios de frontend

---
+++ django/js/script.js
@@ -0,0 +1,15 @@
+function gritar() {
+    alert('AAAAAAAHHHHHHHH')
+}
+
+function perguntar() {
+    var nome;
+    nome = prompt('Qual seu nome? ');
+    alert('ola ' + nome)
+}
+
+function mudar_texto() {
+    var msg;
+    msg = prompt('Digite a mensagem a ser mudada: ')
+    alert(msg)
+}
