e155e544194bc86f61a69f6a671d182bc2dd6a8c
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-01-19 06:23:52 -0300

    Adicionando arquivos de models Produto da aula de Django

--- django/django2/cliente/models.py
+++ django/django2/cliente/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Base(models.Model):
+    criado = models.DateField('Data de criação', auto_now_add=True)
+    modificado = models.DateField('Data de Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
