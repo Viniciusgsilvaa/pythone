095f76536c819284c99460b93f6be3b7dc001ed0
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-01-27 14:43:47 -0300

    Adicionando um model base no projeto da aula

--- django/django3/core/models.py
+++ django/django3/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from stdimage.models import StdImageField
 
-# Create your models here.
+
+class Base(models.Model):
+    criados = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateField('Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
