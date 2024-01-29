b7a8e9d902ef3c7114015ca23a854d98288ffd03
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-01-29 06:41:12 -0300

    Adicionando novos models e migrandoos e criando o admin para cadastrar no banco de dados atraves da pagina admin

--- django/django3/core/admin.py
+++ django/django3/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Servico, Cargo, Equipe
 
-# Register your models here.
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display= ('servico', 'descricao', 'icone', 'criados', 'modificado', 'ativo')
+
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'criados', 'modificado', 'ativo')
+
+
+@admin.register(Equipe)
+class EquipeAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'bio', 'imagem', 'facebook', 'x', 'instagram', 'criados', 'modificado', 'ativo')
