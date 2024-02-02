b5483f2e994655358424f1eac3b4da148af61560
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-02-02 06:46:01 -0300

    Realizando todos os tests

--- django/django3/core/tests/tests_models.py
+++ django/django3/core/tests/tests_models.py
@@ -1,6 +1,6 @@
 import uuid
 from django.test import TestCase
-from model_mommy import model_mommy
+from model_mommy import mommy
 from core.models import get_file_path
 
 
@@ -12,3 +12,31 @@ class GetFilePathTestCase(TestCase):
     def test_get_file_path(self):
         arquivo = get_file_path(None, 'teste.png')
         self.assertTrue(len(arquivo), len(self.filename))
+
+
+class ServicoTestCase(TestCase):
+
+    def setUp(self):
+        self.servico = mommy.make('Servico')
+    
+    def test_str(self):
+        self.assertEquals(str(self.servico), self.servico.servico)
+        
+
+class CargoTestCase(TestCase):
+
+    def setUp(self):
+        self.cargo = mommy.make('Cargo')
+    
+    def test_str(self):
+        self.assertEquals(str(self.cargo), self.cargo.cargo)
+
+
+class EquipeTestCase(TestCase):
+
+    def setUp(self):
+        self.equipe = mommy.make('Equipe')
+    
+    def test_str(self):
+        self.assertEquals(str(self.equipe), self.equipe.nome)
+        
