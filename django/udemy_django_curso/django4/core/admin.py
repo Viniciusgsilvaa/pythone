c64ae5ed529940ad55518c344d4e482b047de573
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-02-24 06:41:03 -0300

    Adicionando novos modelos para o projeto

--- django/udemy_django_curso/django4/core/admin.py
+++ django/udemy_django_curso/django4/core/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
-from .models import Portifolio
+from .models import Portifolio, Cargo, Funcionario, Sobre
 
 
 @admin.register(Portifolio)
 class PortifolioAdmin(admin.ModelAdmin):
     list_display = ('cabecalio', 'sublinha', 'sobre', 'imagem', 'criado', 'modificado', 'ativo')
+
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'criado', 'modificado', 'ativo')
+
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'imagem', 'facebook', 'x', 'instagram', 'criado', 'modificado', 'ativo')
+
+
+@admin.register(Sobre)
+class SobreAdmin(admin.ModelAdmin):
+    list_display = ('data', 'cabecalio', 'sobre', 'imagem', 'criado', 'modificado', 'ativo')
--- django/udemy_django_curso/django4/core/models.py
+++ django/udemy_django_curso/django4/core/models.py
@@ -25,3 +25,45 @@ class Portifolio(Base):
 
     def __str__(self):
         return self.cabecalio
+    
+    class Meta:
+        verbose_name='Portifolio'
+        verbose_name_plural='Portifolios'
+
+
+class Cargo(Base):
+    cargo = models.CharField('Cargo', max_length=50)
+
+    class Meta:
+        verbose_name='Cargo'
+        verbose_name_plural='Cargos'
+
+    def __str__(self):
+        return self.cargo
+
+class Funcionario(Base):
+    nome = models.CharField('Nome', max_length=30)
+    cargo = models.ForeignKey('core.Cargo', verbose_name='Cargo', on_delete=models.CASCADE)
+    imagem = StdImageField('Imagem', upload_to=get_file_path, variations={'thumb': {'width': 500, 'height': 500, 'crop':True}})
+    facebook = models.CharField('Facebook', max_length=100, default='#')
+    x = models.CharField('x', max_length=100, default='#')
+    instagram = models.CharField('Instagram', max_length=100, default='#')
+
+    class Meta:
+        verbose_name='Funcionario'
+        verbose_name_plural='Funcionarios'
+    
+    def __str__(self):
+        return self.nome
+
+class Sobre(Base):
+    data = models.CharField('Data', max_length=50)
+    cabecalio = models.CharField('Cabeçalio', max_length=50)
+    sobre = models.CharField('Sobre', max_length=100)
+    imagem = StdImageField('Imagem', upload_to=get_file_path, variations={'thumb': {'width': 200, 'height': 200, 'crop':True}})
+
+    class Meta:
+        verbose_name='Sobre'
+    
+    def __str__(self):
+        return self.data
