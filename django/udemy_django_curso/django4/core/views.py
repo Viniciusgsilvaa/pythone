9e7ae15d9eef62c19ac5e18810de8150a5fad10e
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-02-26 06:41:26 -0300

    Adicionando context dos funcionarios e extraindo os dados para o Template

--- django/udemy_django_curso/django4/core/views.py
+++ django/udemy_django_curso/django4/core/views.py
@@ -1,5 +1,14 @@
+from typing import Any
 from django.views.generic import TemplateView
+from .models import Sobre, Portifolio, Funcionario
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['sobre'] = Sobre.objects.all()
+        context['portifolio'] = Portifolio.objects.all()
+        context['funcionarios'] = Funcionario.objects.all()
+        return context
