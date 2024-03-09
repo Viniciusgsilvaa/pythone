3133c485317084fb5f78dd3c3b77077b9ff9b80d
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-03-09 14:48:20 -0300

    Finaliznado a aula de crud

--- django/udemy_django_curso/cruddjango/core/views.py
+++ django/udemy_django_curso/cruddjango/core/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Produto
 from .forms import ProdutoModelForm
+from django.urls import reverse_lazy
 
 
 class IndexView(ListView):
@@ -9,3 +10,23 @@ class IndexView(ListView):
     template_name= 'index.html'
     queryset = Produto.objects.all()
     context_object_name = 'produtos'
+
+
+class CreateProdutoView(CreateView):
+    model=Produto
+    template_name = 'produto_form.html'
+    fields = ['nome', 'preco']
+    success_url = reverse_lazy('index')
+
+
+class UpdateProdutoView(UpdateView):
+    model=Produto
+    template_name = 'produto_form.html'
+    fields = ['nome', 'preco']
+    success_url = reverse_lazy('index')
+
+
+class DeleteProdutoView(DeleteView):
+    model = Produto
+    template_name = 'produto_del.html'
+    success_url = reverse_lazy('index')
