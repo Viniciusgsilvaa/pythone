a04186eb59965adbf2b70c59cc272a8584299551
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-03-11 14:12:16 -0300

    Criando Api get

--- REST_API/escola/cursos/models.py
+++ REST_API/escola/cursos/models.py
@@ -27,7 +27,7 @@ class Avaliacao(Base):
     nome = models.CharField(max_length=255)
     email = models.EmailField()
     comentario = models.TextField(blank=True, default='')
-    avalicao = models.DecimalField(max_digits=2, decimal_places=1)
+    avaliacao = models.DecimalField(max_digits=2, decimal_places=1)
 
     class Meta:
         verbose_name = 'Avaliação'
--- REST_API/escola/cursos/views.py
+++ REST_API/escola/cursos/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
 
-# Create your views here.
+
+class CursoAPIView(APIView):
+    """ API de cursos """
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response (serializer.data)
+
+
+class AvaliacaoAPIView(APIView):
+
+    """ API de avaliações """
+    
+    def get(self, request):
+        avaliacao = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacao, many=True)
+        return Response(serializer.data)
