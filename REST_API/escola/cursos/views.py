573bb6db13027a325b0bdf832445a71bb02b9bcc
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-03-12 07:42:09 -0300

    Termindo de criação basica de API REST

--- REST_API/escola/cursos/views.py
+++ REST_API/escola/cursos/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
+from rest_framework import status
 
 
 class CursoAPIView(APIView):
@@ -11,6 +12,11 @@ class CursoAPIView(APIView):
         serializer = CursoSerializer(cursos, many=True)
         return Response (serializer.data)
 
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class AvaliacaoAPIView(APIView):
 
@@ -20,3 +26,9 @@ class AvaliacaoAPIView(APIView):
         avaliacao = Avaliacao.objects.all()
         serializer = AvaliacaoSerializer(avaliacao, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
