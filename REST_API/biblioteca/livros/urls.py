be30439a9699139dad16ea8eccc7cd4aa929bbac
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-03-15 06:47:53 -0300

    reescrevendo codigos de api

--- REST_API/biblioteca/livros/urls.py
+++ REST_API/biblioteca/livros/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import AvaliacaoAPIView, LivroAPIView
+from .views import AvaliacoesAPIView, LivroAPIView, AvaliacaoAPIView, LivrosAPIView
 
 urlpatterns = [
-    path('livro/', LivroAPIView.as_view(), name='livro'),
-    path('avaliacoes/', AvaliacaoAPIView.as_view(), name='avaliacoes')
+    path('livros/', LivrosAPIView.as_view(), name='livros'),
+    path('livro/<int:pk>/', LivroAPIView.as_view(), name='livro'),
+    path('livros/<int:livro_pk>/avaliacoes/', AvaliacoesAPIView.as_view(), name='livro_avaliacao'),
+    path('livros/<int:livro_pk>/avaliacoes/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='curso_avaliacao'),
+    path('avaliacoes/', AvaliacoesAPIView.as_view(), name='avaliacoes'),
+    path('avaliacao/<int:avaliacao_pk>/', AvaliacaoAPIView.as_view(), name='avaliacao')
 ]
--- REST_API/biblioteca/livros/views.py
+++ REST_API/biblioteca/livros/views.py
@@ -1,13 +1,34 @@
 from .models import Livro, Avaliacoes
 from .serializers import AvaliacaoSerializer, LivroSerializer
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 
-class LivroAPIView(generics.ListCreateAPIView):
+class LivrosAPIView(generics.ListCreateAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
 
 
-class AvaliacaoAPIView(generics.ListCreateAPIView):
+class LivroAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+
+
+class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacoes.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_queryset(self):
+        if self.kwargs.get('livro_pk'):
+            return self.queryset.filter(titulo_id=self.kwargs.get('livro_pk'))
+        return self.queryset.all()
+
+class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Avaliacoes.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('livro_pk'):
+            return get_object_or_404(self.get_queryset(), titulo_id=self.kwargs.get('livro_pk'), pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.get_queryset(), pk=self.kwargs.get('avaliacao_pk'))
+ 
