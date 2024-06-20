b2c2761cfa2747fd642c93f87fefac8f645ab90e
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-06-20 20:18:51 -0300

    finaliznado a aula sobre web

--- fast_zero/aulafastapi/aulafastapi/app.py
+++ fast_zero/aulafastapi/aulafastapi/app.py
@@ -1,8 +1,11 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+from aulafastapi.schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "ola mundo"}
+
