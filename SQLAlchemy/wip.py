d395cc9a5ba18597b1a39dbb65322e92d6f430c3
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-06-24 05:42:16 -0300

    SQLAlchemy

--- SQLAlchemy/wip.py
+++ SQLAlchemy/wip.py
@@ -1,8 +1,13 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+
+engine = create_engine('sqlite:///SQLAlchemy/db.sqlite3', echo=True)
+
+with engine.connect() as con:
+    with engine.begin():
+        sql = text('select id, nome, preco from nucleo_produto')
+        result = con.execute(sql)
+    with engine.begin():
+        sql = text('select id, nome, preco from nucleo_produto')
+        result = con.execute(sql)
 
 
-engine = create_engine('sqlite://database.db', echo=True)
-
-con = engine.connect()
-
-con.close()
