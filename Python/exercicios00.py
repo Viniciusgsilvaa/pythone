b90f1b40fdc380395a6387c550f73f71260134cc
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-07-21 20:40:09 -0300

    exercicios de python

--- Python/exercicios00.py
+++ Python/exercicios00.py
@@ -80,4 +80,45 @@ def find_uniq(arr):
         if arr.count(n) == 1:
             return n
 
+"""
+Well met with Fibonacci bigger brother, AKA Tribonacci.
 
+As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+
+So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
+
+[1, 1 ,1, 3, 5, 9, 17, 31, ...]
+But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+
+[0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+
+Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+"""
+
+def tribonacci(s, n):
+    if n == 0:
+        return []
+    elif n <= 3:
+        return s[:n]
+    
+    a = s
+    for x in range(3, n):
+        next = sum(a[-3:])
+        a.append(next)
+    return a
+
+def prod_int_partII(n, s):   # n, integer to have the multiplicative partitions
+    filter = 0
+    qty = 0
+    lista = []
+    while n != 1:
+        for v in range(2, 10):
+            if n % v == 0:
+                lista.append(v)
+                qty += 1
+                n = n/v
+    print(lista)    
+    return [qty, filter ]
+
+print(prod_int_partII(36, 3))
