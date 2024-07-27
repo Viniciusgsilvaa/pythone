3124fcc218db6cb552ead3bbd5ba270232fce7d2
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-07-27 05:47:08 -0300

    exercicios

--- Python/exercicios00.py
+++ Python/exercicios00.py
@@ -108,17 +108,40 @@ def tribonacci(s, n):
         a.append(next)
     return a
 
-def prod_int_partII(n, s):   # n, integer to have the multiplicative partitions
-    filter = 0
-    qty = 0
-    lista = []
-    while n != 1:
-        for v in range(2, 10):
-            if n % v == 0:
-                lista.append(v)
-                qty += 1
-                n = n/v
-    print(lista)    
-    return [qty, filter ]
+"""
+Trolls are attacking your comment section!
 
-print(prod_int_partII(36, 3))
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+"""
+
+def disemvowel(string):
+    a = string
+    for n in 'aeiouAEIOU':
+        a = a.replace(n, '')
+    return a
+
+"""
+Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#, empty table in COBOL) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+Example:
+divisors(12); #should return [2,3,4,6]
+divisors(25); #should return [5]
+divisors(13); #should return "13 is prime"
+"""
+
+def divisors(integer):
+    div = []
+    for n in range(2, integer):
+        if integer % n == 0:
+            div.append(n)
+    if len(div) > 0:
+        return div
+    else:
+        return f'{integer} is prime'
+
