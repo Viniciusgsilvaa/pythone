415ec129f2b9da4e6e67799455a32ee4ebef9ceb
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-08-01 10:44:32 -0300

    exercicios de python

--- Python/exercicios00.py
+++ Python/exercicios00.py
@@ -108,17 +108,60 @@ def tribonacci(s, n):
         a.append(next)
     return a
 
-def prod_int_partII(n, s):   # n, integer to have the multiplicative partitions
-    filter = 0
-    qty = 0
-    lista = []
-    while n != 1:
-        for v in range(2, 10):
-            if n % v == 0:
-                lista.append(v)
-                qty += 1
-                n = n/v
-    print(lista)    
-    return [qty, filter ]
+"""
+Trolls are attacking your comment section!
 
-print(prod_int_partII(36, 3))
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+"""
+
+def disemvowel(string):
+    a = string
+    for n in 'aeiouAEIOU':
+        a = a.replace(n, '')
+    return a
+
+"""
+Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#, empty table in COBOL) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+Example:
+divisors(12); #should return [2,3,4,6]
+divisors(25); #should return [5]
+divisors(13); #should return "13 is prime"
+"""
+
+def divisors(integer):
+    div = []
+    for n in range(2, integer):
+        if integer % n == 0:
+            div.append(n)
+    if len(div) > 0:
+        return div
+    else:
+        return f'{integer} is prime'
+
+"""
+Complete the function which returns the weekday according to the input number:
+
+1 returns "Sunday"
+2 returns "Monday"
+3 returns "Tuesday"
+4 returns "Wednesday"
+5 returns "Thursday"
+6 returns "Friday"
+7 returns "Saturday"
+Otherwise returns "Wrong, please enter a number between 1 and 7"
+"""
+
+def whatday(num):
+    days = ["Wrong, please enter a number between 1 and 7", "Sunday", "Monday", 
+          "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    if num > 7:
+        return "Wrong, please enter a number between 1 and 7"
+    return days[num]
+
