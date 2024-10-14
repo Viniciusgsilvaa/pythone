7ebf2c8877e96a39e0b396e9473be3a2001cf595
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-10-14 12:10:36 +0100

    exercicio de python-djongo-api-blog alguns ajustes

--- django/exercicios/blog/blog/admin.py
+++ django/exercicios/blog/blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Perfil, Seguidor
 
 admin.site.register(Post)
+admin.site.register(Perfil)
+admin.site.register(Seguidor)
+
--- django/exercicios/blog/blog/models.py
+++ django/exercicios/blog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Base(models.Model):
@@ -11,10 +12,39 @@ class Base(models.Model):
         abstract = True
 
 class Post(Base):
-
+    
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     nome = models.CharField('Nome', max_length=200)
     titulo = models.CharField('Titulo', max_length=100)
     conteudo = models.TextField()
 
     def __str__(self):
         return self.nome
+    
+
+class Perfil(Base):
+
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    localizacao = models.CharField(max_length=100)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.usuario.username
+
+    def get_seguindo(self):
+        return User.objects.filter(seguidores__seguidor=self.usuario)
+    
+    def get_seguidores(self):
+        return User.objects.filter(seguindo_usuario=self.usuario)
+
+class Seguidor(Base):
+
+    usuario = models.ForeignKey(User, related_name='seguindo', on_delete=models.CASCADE)
+    seguidor = models.ForeignKey(User, related_name='seguidores', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('usuario', 'seguidor')
+    
+    def __str__(self):
+        return self.usuario.username
