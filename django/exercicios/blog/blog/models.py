cff4ba47cbe90d8ab8302ec56e4aad65344b737a
Vinicius Garcia <vinicius.garcia.dev@gmail.com>
2024-10-15 21:57:07 +0100

    exercicio de python-djongo-api-blog alguns ajustes

--- django/exercicios/blog/blog/models.py
+++ django/exercicios/blog/blog/models.py
@@ -20,7 +20,29 @@ class Post(Base):
 
     def __str__(self):
         return self.nome
+
+
+class Comentario(Base):
+    post = models.ForeignKey(Post,  related_name='comentarios', on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    texto = models.TextField()
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'Comentário de {self.usuario} no post {self.post.titulo}' 
+
+
+class VotoComentario(Base):
+    comentario = models.ForeignKey(Comentario, related_name='votos', on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    tipo_voto = models.CharField(max_length=10, choices=[('like', 'Like'), ('dislike', 'Dislike')])
     
+    class Meta:
+        unique_together = ('comentario', 'usuario')  # Garante que o usuário só possa votar uma vez no comentário
+
+    def __str__(self):
+        return f'{self.usuario} votou {self.tipo_voto} no comentário {self.comentario.id}'
 
 class Perfil(Base):
 
